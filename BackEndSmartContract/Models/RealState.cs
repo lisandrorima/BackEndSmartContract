03da753c229729ab1529db833c45564a371c95a1
Lisandro Rima <lrima@Lenovo.com>
2021-04-15 18:47:41 -0300

    Add project files.

---
+++ BackEndSmartContract/Models/RealState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BackEndSmartContract.Models
+{
+	public class RealState
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int ID { get; set; }
+		public string Adress { get; set; }
+		public int RentFee { get; set; }
+		public int RentDurationDays { get; set; }
+		public int RentPaymentSchedule { get; set; }
+		public float SqMtrs { get; set; }
+		public string Description { get; set; }
+		public int Ambientes { get; set; }
+		public int BedRoomQty { get; set; }
+		public int BathRoomQty { get; set; }
+		public bool Garage { get; set; }
+		public bool Available { get; set; }
+		public User user { get; set; }
+	}
+}
---
+++ BackEndSmartContract/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BackEndSmartContract.Models
+{
+	public class User
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int ID{ get; set; }
+		public int PersonalID { get; set; }
+		public string Email { get; set; }
+
+		[Column(TypeName = "NVARCHAR(100)")]
+		public string Name { get; set; }
+
+		[Column(TypeName = "NVARCHAR(100)")]
+		public string Surname { get; set; }
+
+		[Column(TypeName = "NVARCHAR(42)")]
+		public string WalletAdress { get; set; }
+	}
+}
